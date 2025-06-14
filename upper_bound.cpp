5a95dd4cf0be424f186fd7aafc2eca9fe50602d4
Nishant Mishra <nishantmishra3108@gmail.com>
2025-06-15 01:38:01 +0530

    upper bound

---
+++ upper_bound.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int upper_bound(vector <int> &arr, int target){
+    int n = arr.size();
+    int low = 0, high = n - 1, ans = n;
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if(arr[mid] > target){
+            high = mid - 1;
+            ans = mid;
+        }
+        else low = mid + 1;
+    }
+    return ans;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector <int> arr(n);
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    // call
+    cout << upper_bound(arr, 10);
+    return 0;
+}

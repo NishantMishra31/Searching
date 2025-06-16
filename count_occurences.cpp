8a358436c6827da3bab26a99f2a854a1323bc88d
Nishant Mishra <nishantmishra3108@gmail.com>
2025-06-16 20:36:12 +0530

    Added Files and code changes

--- count_occurences.cpp
+++ count_occurences.cpp
@@ -1,8 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector <int> countOccurences(vector <int> &arr, int target){
+int firstOccurence(vector <int> &arr, int target){
     int n = arr.size();
+    int low = 0, high = n - 1, first = -1, mid;
+    while (low <= high){
+        mid = low + (high - low) / 2;
+        if (arr[mid] == target){
+            first = mid;
+            high = mid - 1;
+        }
+        else if(arr[mid] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return first;
+}
+
+int lastOccurence(vector <int> &arr, int target){
+    int n = arr.size();
+    int low = 0, high = n - 1, last = -1, mid;
+    while (low <= high){
+        mid = low + (high - low) / 2;
+        if (arr[mid] == target){
+            last = mid;
+            low = mid + 1;
+        }
+        else if(arr[mid] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return last;
+}
+
+vector <int> searchRange(vector <int> &arr, int target){
+    int first = firstOccurence(arr, target);
+    if (first == -1) return {-1, -1};
+    int last = lastOccurence(arr, target);
+    return {first, last};
+}
+
+int countOccurences(vector <int> &arr, int target){
+    int first = firstOccurence(arr, target);
+    if (first == -1) return 0;
+    int last = lastOccurence(arr, target);
+    return last - first + 1;
     
 }
 
@@ -16,9 +56,6 @@ int main(){
     int target;
     cin >> target;
     // call
-    vector <int> ans = countOccurences(arr, target);
-    for (auto it : ans){
-        cout << it << " ";
-    }
+    cout << countOccurences(arr, target);
     return 0;
 }

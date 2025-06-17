fb4bb20b3caffba86430f3eda6510b0a63e7c2a3
Nishant Mishra <nishantmishra3108@gmail.com>
2025-06-18 01:39:17 +0530

    Added Files and code changes

---
+++ find_peak_element3.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findPeakElement(vector<int>& nums) {
+    int n = nums.size();
+    int low = 0, high = n - 1;
+
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] < nums[mid + 1])
+            low = mid + 1;
+        else
+            high = mid;
+        }
+    return low;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector <int> arr(n);
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    //call
+    cout << findPeakElement(arr);
+    return 0;
+}

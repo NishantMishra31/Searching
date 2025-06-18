d0052839b395fef77c5f8bbeb23b5938c2a19d78
Nishant Mishra <nishantmishra3108@gmail.com>
2025-06-18 13:54:26 +0530

    Added Files and code changes

---
+++ isPerfectsquare.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPerfectSquare(int num) {
+    if (num < 1) return false;
+    int low = 1, high = num;
+    while (low <= high) {
+        long long mid = low + (high - low) / 2;
+        long long square = mid * mid;
+        if (square == num) return true;
+        else if (square < num) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false;
+    }
+
+int main(){
+    int n;
+    cin >> n;
+    cout << isPerfectSquare(n);
+    return 0;
+}

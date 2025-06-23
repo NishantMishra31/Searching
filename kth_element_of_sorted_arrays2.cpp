c90e62c444f777c5ec74fd76dde56800c0f9f1f4
Nishant Mishra <nishantmishra3108@gmail.com>
2025-06-23 16:03:39 +0530

    Added Files and code changes

--- kth_element_of_sorted_arrays2.cpp
+++ kth_element_of_sorted_arrays2.cpp
@@ -1,59 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// this approach limits the extra space usage and takes an count variable to track the elements indices
-
-double median(vector <int> &arr1, vector <int> &arr2){
+int kth_element(vector<int> &arr1, vector<int> &arr2, int k) {
     int n = arr1.size();
     int m = arr2.size();
-    int i = 0, j = 0;
-    int z = n + m, cnt = 0;
-    int ind2 = z / 2;
-    int ind1 = ind2 - 1;
-    int element1 = -1, element2 = -1;
-    while ( i < n && j < m){
-        if (arr1[i] < arr2[j]){
-            if(cnt == ind1) element1 = arr1[i];
-            if(cnt == ind2) element2 = arr1[i];
-            cnt++;
-            i++;
-        }
-        else {
-            if(cnt == ind1) element1 = arr2[j];
-            if(cnt == ind2) element2 = arr2[j];
-            cnt++;
-            j++;
-        }
-    }
-    while (i < n){
-        if(cnt == ind1) element1 = arr1[i];
-        if(cnt == ind2) element2 = arr1[i];
+    int i = 0, j = 0, cnt = 0;
+
+    while (i < n && j < m) {
+        int val;
+        if (arr1[i] < arr2[j]) val = arr1[i++];
+        else val = arr2[j++];
         cnt++;
+        if (cnt == k) return val;
+    }
+
+    while (i < n) {
+        cnt++;
+        if (cnt == k) return arr1[i];
         i++;
     }
-    while (j < m){
-        if(cnt == ind1) element1 = arr2[j];
-        if(cnt == ind2) element2 = arr2[j];
+
+    while (j < m) {
         cnt++;
+        if (cnt == k) return arr2[j];
         j++;
     }
-    if (z % 2 == 0) return ((double)(element1 + element2) / 2);
-    else return element2;
-    return 0;
+    return -1;
 }
 
-int main(){
+int main() {
     int n, m;
     cin >> n >> m;
-    vector <int> arr1(n);
-    vector <int> arr2(m);
-    for (int i = 0; i < n; i++){
-        cin >> arr1[i];
-    }
-    for (int i = 0; i < m; i++){
-        cin >> arr2[i];
-    }
+    vector<int> arr1(n), arr2(m);
+    for (int i = 0; i < n; i++) cin >> arr1[i];
+    for (int i = 0; i < m; i++) cin >> arr2[i];
+    int k;
+    cin >> k;
     // call
-    cout << median(arr1, arr2);
+    cout << kth_element(arr1, arr2, k);
     return 0;
 }

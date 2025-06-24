95466af3b5dddfd1ccc2299081ce99e3161e7084
Nishant Mishra <nishantmishra3108@gmail.com>
2025-06-24 13:19:59 +0530

    Added Files and code changes

--- search_2d_matrix2.cpp
+++ search_2d_matrix2.cpp
@@ -1,13 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool bs(vector<int> mat, int k){
+    int x = mat.size();
+    int low = 0, high = x - 1;
+    while (low <= high){
+        int mid = low + (high - low) / 2;
+        if (mat[mid] == k) return true;
+        else if(mat[mid] < k){
+            low = mid + 1;
+        }
+        else high = mid - 1;
+    }
+    return false;
+}
+
 bool search(vector<vector<int>> mat, int k){
     int m = mat.size();
     int n = mat[0].size();
     int index = -1, max_count = 0;
     for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            if (mat[i][j] == k) return true;
+        if(mat[i][0] <= k && k <= mat[i][n-1]){
+            return bs(mat[i], k);
         }
     }
     return false;

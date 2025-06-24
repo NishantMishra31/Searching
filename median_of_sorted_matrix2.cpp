003d2cd6a1bab3dcda5b869b3f0e8135cbdd14fd
Nishant Mishra <nishantmishra3108@gmail.com>
2025-06-25 01:52:40 +0530

    Added Files and code changes

--- median_of_sorted_matrix2.cpp
+++ median_of_sorted_matrix2.cpp
@@ -1,33 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-// naive solution 
+// this approach is optimal and uses binary search adaptation
 
-int median(vector<vector<int>> &mat){
-    int m = mat.size();
-    int n = mat[0].size();
-    vector <int> arr;
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            arr.push_back(mat[i][j]);
-        }
-    }
-    sort(arr.begin(), arr.end());
-    return arr[(m * n) / 2];
+int countLessEqual(const vector<int>& row, int target) {
+    return upper_bound(row.begin(), row.end(), target) - row.begin();
 }
 
-int main(){
+int findMedian(vector<vector<int>> &mat) {
+    int m = mat.size();
+    int n = mat[0].size();
+    int low = INT_MAX, high = INT_MIN;
+    for (int i = 0; i < m; i++) {
+        low = min(low, mat[i][0]);
+        high = max(high, mat[i][n - 1]);
+    }
+    int desired = (m * n + 1) / 2;
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            count += countLessEqual(mat[i], mid);
+        }
+        if (count < desired)
+            low = mid + 1;
+        else
+            high = mid;
+    }
+    return low;
+}
+
+int main() {
     int m, n;
     cin >> m >> n;
     vector<vector<int>> mat(m, vector<int>(n));
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
             cin >> mat[i][j];
-        }
-    }
-    int k;
-    cin >> k;
     //call
-    cout << median(mat);
+    cout << findMedian(mat);
     return 0;
 }
